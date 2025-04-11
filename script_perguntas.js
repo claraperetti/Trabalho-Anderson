0dbaf56bae23d82b600b07ecfd6b1ef37b983955
Clara L Peretti <claralobrigatte@gmail.com>
2025-04-11 14:29:40 -0300

    insert-tela-final

--- script_perguntas.js
+++ script_perguntas.js
@@ -1,3 +1,18 @@
 let jsonQuestions = localStorage.getItem("jsonQuestions")
 
-console.log(jsonQuestions)
+let parsedQuestions = JSON.parse(jsonQuestions);
+
+class Question {
+     constructor( question_id, text, responses_array, correct_answer) {
+        this.question_id = question_id
+        this.text = text
+        this.responses_array = responses_array
+        this.correct_answer = correct_answer
+     }
+}
+
+jsonQuestions.array.forEach(element => {
+    let question = new Question(element.idQuestion, element.text, element.response, element.correctAnswer)
+    console.log(question)
+});
+
